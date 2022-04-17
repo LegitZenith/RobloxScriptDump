cd872bafdffb45f84de906f21e87f623d83aacb5
Sy 3 <64887477+LegitZenith@users.noreply.github.com>
2022-04-17 01:46:35 -0400

    Update!
    More Scripts Were Added! Check them out!

---
+++ DaHood.lua
@@ -0,0 +1,5 @@
+--BROUGHT TO YOU BY RSCRIPTS.NET!--
+
+-- / / Main Script | Destiny #1 | Da Hood
+-- / / V to open and close
+loadstring(game:HttpGet('https://raw.githubusercontent.com/compwnter/destiny/main/loader'))();
